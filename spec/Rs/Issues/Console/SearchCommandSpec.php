159e573456d8bbf9e525739df2e6bd6ac977756d
Robert Schönthal <caziel@gmx.net>
2015-09-03 22:17:49 +0200

    fixed gitlab tests

--- spec/Rs/Issues/Console/SearchCommandSpec.php
+++ spec/Rs/Issues/Console/SearchCommandSpec.php
@@ -24,7 +24,7 @@ class SearchCommandSpec extends ObjectBehavior
 
     public function it_displays_a_search_result_table_for_gitlab()
     {
-        $input = new ArrayInput(array('type' => 'gitlab', 'project' => 'gitlab/*', '-d'=>'http://demo.gitlab.com/api/v3/', '-u'=>'CPcomi7q3qyREs8wkpQz'));
+        $input = new ArrayInput(array('type' => 'gitlab', 'project' => 'gitlab-org/*', '-d'=>'https://gitlab.com/api/v3/', '-u' => getenv('GITLAB_KEY')));
         $output = new BufferedOutput();
 
         $this->run($input, $output)->shouldPrintATable($output);
